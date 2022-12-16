01827ecf18983ac11a5c7f5803687f4dedbd5275
hongsoom <hongsoom@kakao.com>
2022-12-16 13:47:21 +0900

    [feat] 절차지향 프로그래밍

--- 3-OOP/3-1-without-oop.ts
+++ 3-OOP/3-1-without-oop.ts
@@ -1,21 +1,21 @@
 {
   type CoffeeCup = {
-    shots: number;
-    hasMilk: boolean;
-  };
+    shots: number; // 얼마나 많은 샷이 들어있는 커피인지
+    hasMilk: boolean; // 우유가 들어있는 커피인지
+  }
 
-  const BEANS_GRAMM_PER_SHOT: number = 7;
+  const BEANS_GRAMM_PER_SHOT: number = 7; // 한 샷당 커피콩이 얼마나 들어가는지(중량)
+  let coffeeBeans: number = 0; // 현재 커피콩이 얼마나 있는지
 
-  let coffeeBeans: number = 0;
   function makeCoffee(shots: number): CoffeeCup {
     if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
       throw new Error('Not enough coffee beans!');
     }
     coffeeBeans -= shots * BEANS_GRAMM_PER_SHOT;
     return {
-      shots,
+      shots: shots, // key와 value 이름이 동일하다면 생략 가능 shots,
       hasMilk: false,
-    };
+    }
   }
 
   coffeeBeans += 3 * BEANS_GRAMM_PER_SHOT;
