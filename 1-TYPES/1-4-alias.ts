331fe2fbb184948ca26888adc93c41bb77c616ca
hongsoom <hongsoom@kakao.com>
2023-01-25 16:59:16 +0900

    [Feat] Alias Type 예시

--- 1-TYPES/1-4-alias.ts
+++ 1-TYPES/1-4-alias.ts
@@ -15,9 +15,42 @@
         // 정의한 데이터를 꼭 써줘야함 안그러면 오류 
     };
 
+    // 똑같은 내용을 반복하지 않게 Alias(별칭)타입 생성
+    type Player = {
+        name: string,
+        age?: number
+    }
+
+    const sumin: Player = {
+        name: "sumin"
+    }
+
+    const nico: Player = {
+        name: "nico",
+        age: 12
+    }
+
+    // 정리
+    type Age = number;
+    type Name = string;
+
+    type Player1 = {
+        name: Name,
+        age?: Age
+    }
+
+    const sumin1: Player1 = {
+        name: "sumin"
+    }
+
+    const nico2: Player1 = {
+        name: "nico",
+        age: 12
+    }
+
     // String Literal Types - 타입을 문자열로 정의
-    type Name = "name";
-    let suminName: Name;
+    type Name1 = "name";
+    let suminName: Name1;
     suminName = 'name'; // 어떠한 문자열을 넣어서는 안됨 정의된 'name'만 가능
 
     type JSON = 'json';
